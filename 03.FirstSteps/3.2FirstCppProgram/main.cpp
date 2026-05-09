efeeeca5727d1388eb38084e8708783b7c5346c2
MuhammadTahirHussain-Official <mtahirhussainofficial@gmail.com>
2026-05-09 21:39:13 +0500

    initial commit

---
+++ 03.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// struct test
+// {
+//     private:
+//         int x;
+//     public:
+//         int y;
+//         test(int a){
+//             x = a;
+//         }
+//         void get_x()
+//         {
+//             std::cout << x << std::endl;
+//         }
+//         ~test(){
+            
+//         }
+// };
+
+
+int main(){
+
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+
+    // test a(2);
+    // a.get_x();
+
+
+    return 0;
+}
