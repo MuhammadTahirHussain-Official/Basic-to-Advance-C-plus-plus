b7c78b118260b20df63252ae9b59e84c26d36898
MuhammadTahirHussain-Official <mtahirhussainofficial@gmail.com>
2026-09-11 23:25:45 +0500

    initial commit

---
+++ 14.References/14.5.ReferencesWithRangeBasedForLoops/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+int main(){
+
+    int scores[] {1,2,3,4,5,6,7,8,9,10};
+    
+    //Printing positions
+    std::cout << std::endl;
+	
+	std::cout << "Scores : ";
+    for ( auto score : scores){
+        std::cout << " " << score ;
+    }
+	std::cout << std::endl;
+
+
+    std::cout << std::endl;
+	std::cout << "Scores : ";
+    for ( auto& score : scores){
+        score = score * 10;
+        std::cout << score << " ";
+    }
+    std::cout << std::endl;
+
+    //Printing after change
+    std::cout << std::endl;
+	std::cout << "Scores : ";
+    for (auto&& score : scores) {
+        std::cout << score << " ";
+    }
+	std::cout << std::endl;
+    
+    return 0;
+}
